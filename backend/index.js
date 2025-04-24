a6f7ff41385a1ecf9adedf5f97e009f53ed206be
unknown <melkywafula29@gmail.com>
2025-04-24 10:33:32 +0300

    Initial commit with frontend folder

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const productRoutes = require('./routes/products');
+app.use('/api/products', productRoutes);
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+app.listen(5000, () => console.log("Server running on port 5000"));
